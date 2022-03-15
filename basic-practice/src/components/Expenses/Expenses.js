7eb5faa4f8b07196968f661ccd330423d33f8f7e
ddock4you <ddock4you@gmail.com>
2022-03-15 23:37:36 +0900

    section3 finish

--- basic-practice/src/components/Expenses/Expenses.js
+++ basic-practice/src/components/Expenses/Expenses.js
@@ -1,21 +1,32 @@
+import { Fragment, useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpensesFilter from "./ExpensesFilter";
 
 import './Expenses.css';
 import './ExpenseItem.css';
 
 const Expenses = ({expenses}) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (value) => {
+        setFilteredYear(value);
+    }
+
+
     return (
-        <Card className="expenses">
-            {expenses.map(({title, amount, date}, i) => {
-                return (<ExpenseItem
-                    title={title}
-                    amount={amount}
-                    date={date}
-                    key={i}
-                />)
-            })}
-        </Card>
+        <Fragment>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <Card className="expenses">
+                {expenses.map(({title, amount, date}, i) => {
+                    return (<ExpenseItem
+                        title={title}
+                        amount={amount}
+                        date={date}
+                        key={i}
+                    />)
+                })}
+            </Card>
+        </Fragment>
     )
 }
 
