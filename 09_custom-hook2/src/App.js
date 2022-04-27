b9bc8f1e6c0d85c57dc67b420d2c572da5a46548
ddock4you <ddock4you@gmail.com>
2022-04-27 23:58:18 +0900

    custom-hook2 -ing

--- 09_custom-hook2/src/App.js
+++ 09_custom-hook2/src/App.js
@@ -5,28 +5,25 @@ import NewTask from "./components/NewTask/NewTask";
 import useHttp from "./hooks/use-http";
 
 function App() {
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+    const [tasks, setTasks] = useState([]);
     const convertData = (data) => {
         const loadedTasks = [];
-
         for (const taskKey in data) {
             loadedTasks.push({ id: taskKey, text: data[taskKey].text });
         }
+        setTasks(loadedTasks);
     };
-    const {
-        isLoading,
-        error,
-        sendRequest: fetchTasks,
-    } = useHttp(
-        {
-            url: "https://practice-http-react-default-rtdb.firebaseio.com//tasks.json",
-        },
-        convertData
-    );
-    const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
-        fetchTasks();
-    }, []);
+        fetchTasks(
+            {
+                url:
+                    "https://practice-http-react-default-rtdb.firebaseio.com//tasks.json",
+            },
+            convertData
+        );
+    }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
         setTasks((prevTasks) => prevTasks.concat(task));
