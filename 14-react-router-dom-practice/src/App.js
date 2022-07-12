ec749c905cb1446ff3a1c17174dbd3fc0ec836a5
ddock4you <ddock4you@codeline.kr>
2022-07-12 12:14:20 +0900

    [추가] 14 - layout, header 컴포넌트 추가

--- 14-react-router-dom-practice/src/App.js
+++ 14-react-router-dom-practice/src/App.js
@@ -4,27 +4,30 @@ import Nothing from "./components/pages/Nothing";
 import QuotesDetail from "./components/pages/QuotesDetail";
 import Quotes from "./components/pages/Quotes";
 import NewQuote from "./components/pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <Redirect to="/nothing" />
-            </Route>
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/nothing" />
+                </Route>
 
-            <Route path="/quotes" exact>
-                <Quotes />
-            </Route>
-            <Route path="/quotes/:id">
-                <QuotesDetail />
-            </Route>
-            <Route path="/new_quote">
-                <NewQuote />
-            </Route>
-            <Route to="/nothing">
-                <Nothing />
-            </Route>
-        </Switch>
+                <Route path="/quotes" exact>
+                    <Quotes />
+                </Route>
+                <Route path="/quotes/:id">
+                    <QuotesDetail />
+                </Route>
+                <Route path="/new_quote">
+                    <NewQuote />
+                </Route>
+                <Route to="/nothing">
+                    <Nothing />
+                </Route>
+            </Switch>
+        </Layout>
     );
 }
 
