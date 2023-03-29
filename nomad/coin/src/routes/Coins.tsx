2bd92158fd0b26715a05aca19f6e173c8b18333f
ddock4you <ddock4you@gmail.com>
2023-03-29 23:59:06 +0900

    coins 페이지 fetch 코드를 react-query로 변경

--- nomad/coin/src/routes/Coins.tsx
+++ nomad/coin/src/routes/Coins.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useQuery } from "react-query";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -62,26 +63,28 @@ interface CoinInterface {
 }
 
 function Coins() {
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        (async () => {
-            const response = await fetch("https://api.coinpaprika.com/v1/coins");
-            const json = await response.json();
-            setCoins(json.slice(0, 100));
-            setLoading(false);
-        })();
-    }, []);
+    const { isLoading, data: coins } = useQuery<CoinInterface[]>("allCoins", fetchCoins);
+
+    // const [coins, setCoins] = useState<CoinInterface[]>([]);
+    // const [loading, setLoading] = useState(true);
+    // useEffect(() => {
+    //     (async () => {
+    //         const response = await fetch("https://api.coinpaprika.com/v1/coins");
+    //         const json = await response.json();
+    //         setCoins(json.slice(0, 100));
+    //         setLoading(false);
+    //     })();
+    // }, []);
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            {loading ? (
+            {isLoading ? (
                 <Loader>Loading...</Loader>
             ) : (
                 <CoinsList>
-                    {coins.map((coin) => (
+                    {coins?.slice(0, 100).map((coin) => (
                         <Coin key={coin.id}>
                             <Link
                                 to={{
